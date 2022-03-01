636afe3ecf739915cd67af03912721cf3e67a2d9
francesco.farinola <farinola@live.it>
2022-03-01 21:18:16 +0100

    Added solver and util functions

---
+++ CP/solver.py
@@ -0,0 +1,74 @@
+import re
+
+from utils import input, output
+import os
+import sys
+import argparse
+import datetime
+from minizinc import Instance, Model, Solver, Status
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--instances', help="Instances to be solved. Can be a list of instances as '1,3,7' or `all`", default="all")
+    parser.add_argument('-o', '--output', help="Output directory of the files containing the solutions", default='out')
+    parser.add_argument('-t', '--timeout', help="Timeout for the execution of the solvers in seconds", default=300)
+    parser.add_argument('-r', '--rotation', help="Enables rotation mode", default=False, action='store_true')
+    parser.add_argument('-p', '--plot', help="Whether to plot the circuits or not - plots of solved instances will be saved in the directory out-img", default=False, action='store_true')
+    args = parser.parse_args()
+    print(args)
+
+    path = '../instances/'
+    if 'all' in args.instances:
+        input_files = [path + i for i in os.listdir(path)]
+        #inputs = input.convert_instances(path, input_files)
+    else:
+        input_files = [path + 'ins-' + i + '.txt' for i in sorted(args.instances.split(','), key=int)]
+        #inputs = input.convert_instances(path, input_files)
+
+    if not os.path.isdir(args.output):
+        os.mkdir(args.output)
+
+    if not args.rotation:
+        model = 'model.mzn'
+    else:
+        model = 'model_rotation.mzn'
+
+    model = Model(model)
+    gecode = Solver.lookup("gecode")
+
+    times = []
+    solved = []
+    not_solved = []
+    non_optimal = []
+    out_paths = []
+    for i in input_files:
+        instance_n = re.findall(r'\d+', i)[0]
+        plate_w, n, widths, heights = input.read_instance(i)
+        instance = Instance(gecode, model)
+        instance["plate_w"] = plate_w
+        instance["n"] = n
+        instance["width"] = widths
+        instance["height"] = heights
+        result = instance.solve(timeout=datetime.timedelta(seconds=args.timeout))
+        if result.status is Status.OPTIMAL_SOLUTION or Status.SATISFIED:
+            print("Problem {0} solved in {1}s".format(i, result.statistics['time'].total_seconds()))
+            out_file = output.write_output_file(args.output, i, result, instance, args.rotation)
+            chip, circuits = output.load_solution(out_file)
+            if args.plot:
+                if args.rotation:
+                    title = "Instance {0} with rotated circuits".format(instance_n)
+                else:
+                    title = "Instance {0}".format(instance_n)
+                output.plot_grid(chip, circuits, title, i)
+            times.append(result.statistics['time'].total_seconds())
+            solved.append(instance_n)
+            if result.status is Status.SATISFIED:
+                non_optimal.append(instance_n)
+        elif result.status is Status.UNKNOWN:
+            print("Problem {0} not solved within the time limit {1}".format(i, args.timeout))
+            not_solved.append(instance_n)
+    mean_time = round(sum(times)/len(times), 4)
+    print("Solved instances: {0} - Total ({1}/{2})\nExecution times (in seconds): {3}\n"
+          "Mean time: {4}\nNot solved: {5}\nNon optimal: {6}"
+          "".format(solved, len(solved), len(input_files), times, mean_time, not_solved, non_optimal))
